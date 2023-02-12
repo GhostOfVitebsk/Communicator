df8605a24b38f3b10959c1917504f1dce2620ff2
GhostOfVitebsk <124182670+GhostOfVitebsk@users.noreply.github.com>
2023-02-12 13:00:46 +0300

    Add files via upload

---
+++ Client/client_funcs_test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "funcs.h"
+using namespace std;
+
+int main(){
+    Client(10001);
+    return 0;
+}
--- Client/funcs.h
+++ Client/funcs.h
@@ -1,9 +1,33 @@
 #ifndef FUNCS_H
 #define FUNCS_H
 
-#include <wx/wx.h>
+//#include <wx/wx.h>
+#include <sys/types.h>
+#include <iostream>
+#include <sys/socket.h>
+#include <string>
 
-class MyApp: public wxApp{
+using namespace std;
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Здесь функционал
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void Client(int port);
+void Server(int port);
+void Serve(int fd1, int fd2);
+void Write(int fd);
+void Read(int fd);
+void Listen(int sockfd, int backlog);
+int Socket(int domain, int type, int protocol);
+void Inet_pton(int af, const char *src, void *dst);
+int Accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
+void Bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+void Connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Здесь все про GUI
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*class MyApp: public wxApp{
 public:
     virtual bool OnInit();
 };
@@ -19,8 +43,10 @@ public:
     void Click2(wxCommandEvent& event);
     void Clear(wxCommandEvent& event);
     void Select();
-    void Chat();
+    void Chat(wxString str);
     wxTextCtrl *MainEditBox;
+    wxTextCtrl *MsgHistory;
+    wxTextCtrl *ChatHistory;
     wxGridSizer* gridSizer;
     wxBoxSizer *topsizer;
     wxStaticText *text1;
@@ -33,7 +59,7 @@ public:
     wxTextCtrl *tc;
     wxTextCtrl *tc1;
     wxMenu *file;
-}; 
+};*/ 
 
 #endif
 
